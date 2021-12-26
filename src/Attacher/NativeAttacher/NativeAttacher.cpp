2f8660cd26aeba3c2792f38702f857e6687f73f9
Olivier Giniaux <oginiaux@smartadserver.com>
2021-12-26 01:17:35 +0100

    Some cleanup

--- src/Attacher/NativeAttacher/NativeAttacher.cpp
+++ src/Attacher/NativeAttacher/NativeAttacher.cpp
@@ -7,157 +7,169 @@
 
 #pragma comment(lib, "mscoree.lib")
 
-HANDLE GetProcessByID(const DWORD id)
+#define TRY(x)  { \
+					HRESULT hr; \
+					if (FAILED(hr = x)) { \
+						printf("Failed: '%s'. Reason: 0x%x.\n", #x, hr); \
+						return; \
+					} \
+				}
+
+HRESULT GetProcessByID(const DWORD id, HANDLE &processHandle)
 {
-	return OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, id);
+	//processHandle = OpenProcess(PROCESS_VM_READ | PROCESS_QUERY_INFORMATION, FALSE, id);
+	processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, id);
+	if (processHandle == NULL) {
+		return E_POINTER;
+	} else {
+		return S_OK;
+	}
 }
 
-HANDLE GetProcessByName(const TCHAR element[])
+HRESULT GetProcessByName(const TCHAR element[], HANDLE &processHandle)
 {
-    DWORD aProcesses[1024], cbNeeded, cProcesses;
+    DWORD aProcesses[2048], cbNeeded, cProcesses;
     if (!EnumProcesses(aProcesses, sizeof(aProcesses), &cbNeeded))
         return NULL;
 
     // Calculate how many process identifiers were returned.
-    cProcesses = cbNeeded / sizeof(DWORD);
+	cProcesses = cbNeeded / sizeof(DWORD);
 
     // Print the name and process identifier for each process.
     for (unsigned int i = 0; i < cProcesses; i++)
     {
         DWORD dwProcessID = aProcesses[i];
+
         // Get a handle to the process.
-        HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, dwProcessID);
+		if (FAILED(GetProcessByID(dwProcessID, processHandle))) {
+			//wprintf(L"Failing getting process handle for PID: %d\n", dwProcessID);
+			continue;
+		}
 
-        // Get the process name.
-        TCHAR szEachProcessName[MAX_PATH];
-        if (hProcess != NULL)
-        {
-            HMODULE hMod;
-            DWORD cbNeeded;
+		TCHAR szEachProcessName[MAX_PATH] = TEXT("<unknown>");
+		HMODULE hMod;
+		DWORD cbNeeded;
 
-            if (EnumProcessModules(hProcess, &hMod, sizeof(hMod), &cbNeeded))
-            {
-                GetModuleBaseName(hProcess, hMod, szEachProcessName, sizeof(szEachProcessName) / sizeof(TCHAR));
-            }
-        }
+		if (!EnumProcessModules(processHandle, &hMod, sizeof(hMod), &cbNeeded)) {
+			//wprintf(L"Failing enum PID: %d. Error code: %d\n", dwProcessID, GetLastError());
+			continue;
+		}
+
+		// Get the process name.
+		if (!GetModuleBaseName(processHandle, hMod, szEachProcessName, sizeof(szEachProcessName) / sizeof(TCHAR))) {
+			//wprintf(L"Failing getting name for PID: %d. Error code: %d\n", dwProcessID, GetLastError());
+			continue;
+		}
+
+		//printf("Process name: %s\n", szEachProcessName);
 
         // if they dont match, exit. otherwise get this party started
         if (_tcscmp(element, szEachProcessName) == 0) {
-            return hProcess;
+            return S_OK;
         }
 
-        CloseHandle(hProcess);
+        CloseHandle(processHandle);
     }
 
-    return NULL;
+    return E_POINTER;
 }
 
 void AttachToProcess(LPCWSTR sz_runtimeVersion, HANDLE handle) {
 	ICLRMetaHost* pMetaHost = NULL;
 	ICLRRuntimeInfo* pRuntimeInfo = NULL;
 	ICLRProfiling* pClrProfiling = NULL;
-	HRESULT hr;
-	BOOL bLoadable;
 
-	/* Get ICLRMetaHost instance */
-	if (FAILED(CLRCreateInstance(CLSID_CLRMetaHost, IID_ICLRMetaHost, (VOID**)&pMetaHost))) {
-		printf("[!] CLRCreateInstance(...) failed\n");
+	TRY(CLRCreateInstance(CLSID_CLRMetaHost, IID_ICLRMetaHost, (VOID**)&pMetaHost));
+	TRY(pMetaHost->GetRuntime(sz_runtimeVersion, IID_ICLRRuntimeInfo, (VOID**)&pRuntimeInfo));
+
+	//ICLRRuntimeHost* runtimeHost = NULL;
+	//TRY(pRuntimeInfo->GetInterface(CLSID_CLRRuntimeHost, IID_ICLRRuntimeHost, (LPVOID*)&runtimeHost));
+	//// Start runtime, and load our assembly
+	//runtimeHost->Start();
+
+	//BOOL isStarted;
+	//DWORD startedFlags;
+	//TRY(pRuntimeInfo->IsStarted(&isStarted, &startedFlags));
+	//if (!isStarted) {
+	//	printf("Runtime is not started!\n");
+	//	return;
+	//}
+
+	BOOL isLoadable;
+	TRY(pRuntimeInfo->IsLoadable(&isLoadable));
+	if (!isLoadable) {
+		printf("Runtime is not loadable!\n");
 		return;
 	}
 
-	/* Get ICLRRuntimeInfo instance */
-	if (FAILED(pMetaHost->GetRuntime(sz_runtimeVersion, IID_ICLRRuntimeInfo, (VOID**)&pRuntimeInfo))) {
-		printf("[!] pMetaHost->GetRuntime(...) failed\n");
-		return;
-	}
+	//BOOL isLoaded;
+	//TRY(pRuntimeInfo->IsLoaded(handle, &isLoaded));
+	//if (!isLoaded) {
+	//	printf("Process is not loaded in this runtime!\n");
+	//	return;
+	//}
 
-	/* Check if the specified runtime can be loaded */
-	if (FAILED(pRuntimeInfo->IsLoadable(&bLoadable)) || !bLoadable) {
-		printf("[!] pRuntimeInfo->IsLoadable(...) failed\n");
-		return;
-	}
-
-	if (FAILED(pRuntimeInfo->IsLoaded(handle, &bLoadable))) {
-		printf("[!] This runtime is not loaded in this process\n");
-		return;
-	}
-
-	/* Get ICLRProfiling instance */
-	if (FAILED(hr = pRuntimeInfo->GetInterface(CLSID_CLRProfiling, IID_ICLRProfiling, (LPVOID*)&pClrProfiling))) {
-		printf("[!] Failed getting profiling interface. Error: 0x % x.\n", hr);
-		return;
-	}
+	TRY(pRuntimeInfo->GetInterface(CLSID_CLRProfiling, IID_ICLRProfiling, (LPVOID*)&pClrProfiling));
 
 	DWORD id = GetProcessId(handle);
 
-	BSTR GuidName = SysAllocString(L"{BD097ED8-733E-43FE-8ED7-A95FF9A8448C}");
-	IID Guid;
-	if (FAILED(IIDFromString(GuidName, &Guid)))
-	{
-		printf("[!] Failed retriving profiler GUID\n");
-		return;
+	CLSID clsidProfiler;
+	TRY(CLSIDFromString(L"{BD097ED8-733E-43FE-8ED7-A95FF9A8448C}", (LPCLSID)&clsidProfiler));
+
+	auto path = L"C:\\Users\\oginiaux\\Projects\\traceman\\src\\x64\\Debug\\Profiler.Windows.dll";
+	LPVOID pvClientData = NULL;
+	DWORD cbClientData = 0;
+
+	TRY(pClrProfiling->AttachProfiler(id, 10000, &clsidProfiler, path, pvClientData, cbClientData));
+}
+
+void PrintRuntimes(IEnumUnknown* &enumRuntimes) {
+	IUnknown* enumRuntime = NULL;
+	ICLRRuntimeInfo* runtimeInfo = NULL;
+	LPWSTR frameworkName = (LPWSTR)LocalAlloc(LPTR, 2048);
+	DWORD bytes = 2048, result = 0;
+
+	while (enumRuntimes->Next(1, &enumRuntime, 0) == S_OK) {
+		TRY(enumRuntime->QueryInterface<ICLRRuntimeInfo>(&runtimeInfo));
+		TRY(runtimeInfo->GetVersionString(frameworkName, &bytes));
+		wprintf(L"- %s\n", frameworkName);
+	}
+}
+
+void Attach() {
+	// Get managed process handle
+	HANDLE processHandle;
+	TRY(GetProcessByName(TEXT("Fibonacci.exe"), processHandle));
+	//TRY(GetProcessByID(136620, processHandle));
+
+	if (processHandle == NULL) {
+		printf("Failed getting process handle\n");
 	}
 
-	LPCWSTR path = L"C:\\Users\\oginiaux\\Projects\\DotNextMoscow2019\\x64\\Debug\\DotNext.Profiler.Windows.dll";
+	// Get metahost
+	HMODULE hModule = LoadLibrary(TEXT("mscoree.dll"));
+	CreateInterfaceFnPtr createInterface = (CreateInterfaceFnPtr)GetProcAddress(hModule, "CreateInterface");
+	ICLRMetaHost* pMetaHost = NULL;
+	TRY(createInterface(CLSID_CLRMetaHost, IID_ICLRMetaHost, (LPVOID*)&pMetaHost));
 
-	if (FAILED(hr = pClrProfiling->AttachProfiler(id, 10000, &Guid, path, NULL, 0))) {
-		printf("[!] Failed attaching profiler. Error: 0x%x.\n", hr); // file not found...
-		return;
-	}
+	// Get some runtime information
+	IEnumUnknown* enumLoadedRuntimes = NULL;
+	IEnumUnknown* enumInstalledRuntimes = NULL;
+	TRY(pMetaHost->EnumerateLoadedRuntimes(processHandle, &enumLoadedRuntimes));
+	TRY(pMetaHost->EnumerateInstalledRuntimes(&enumInstalledRuntimes));
 
-	printf("ATTACHED!");
+	printf("Loaded Runtimes:\n");
+	PrintRuntimes(enumLoadedRuntimes);
+
+	printf("Installed Runtimes:\n");
+	PrintRuntimes(enumInstalledRuntimes);
+
+	//AttachToProcess(L"v2.0.50727", processHandle);
+	AttachToProcess(L"v4.0.30319", processHandle);
 }
 
 int main()
 {
-    std::cout << "Hello World!\n";
-
-	HMODULE hModule = LoadLibrary(L"mscoree.dll");
-	CreateInterfaceFnPtr createInterface = (CreateInterfaceFnPtr)GetProcAddress(hModule, "CreateInterface");
-
-	ICLRMetaHost* pMetaHost = NULL;
-	HRESULT hr = createInterface(CLSID_CLRMetaHost, IID_ICLRMetaHost, (LPVOID*)&pMetaHost);
-
-    HANDLE processHandle = GetProcessByName(L"MyDotnetApplication.exe");
-
-	//Installed: v2.0.50727
-    //Installed: v4.0.30319
-	AttachToProcess(L"v4.0.30319", processHandle);
-
-	std::cin.get();
-
-    if (processHandle == NULL) {
-        std::cout << "Failed getting process handle!\n";
-    }
-
-	IEnumUnknown* pEnumUnknown = NULL;
-	hr = pMetaHost->EnumerateLoadedRuntimes(processHandle, &pEnumUnknown);
-
-    if (SUCCEEDED(hr)) {
-        std::cout << "Failed getting loaded runtimes!\n";
-    }
-
-    pMetaHost->EnumerateInstalledRuntimes(&pEnumUnknown);
-
-	IUnknown* enumRuntime = NULL;
-	ICLRRuntimeInfo* runtimeInfo = NULL;
-	LPWSTR frameworkName = NULL;
-	DWORD bytes = 2048, result = 0;
-
-    wprintf(L"Runtimes:\n");
-
-	// Enumerate through runtimes and show supported frameworks
-	while (pEnumUnknown->Next(1, &enumRuntime, 0) == S_OK) {
-		if (enumRuntime->QueryInterface<ICLRRuntimeInfo>(&runtimeInfo) == S_OK) {
-			if (runtimeInfo != NULL) {
-				runtimeInfo->GetVersionString((LPWSTR)&frameworkName, &bytes);
-				std::cout << "[*] Supported Framework: " << frameworkName;
-                wprintf(L"Formatted message: %s\n", frameworkName);
-			}
-		}
-	}
-
-    ICorRuntimeHost* pRuntimeHost = NULL;
-
+	Attach();
 	std::cin.get();
 }
