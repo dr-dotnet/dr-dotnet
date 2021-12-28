1cea39f3694d906d65e1d5c60e4f907d76af685a
Olivier Giniaux <oginiaux@smartadserver.com>
2021-12-28 01:24:19 +0100

    Finally working on dotnet core

---
+++ src/ManagedAttachedNew/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Diagnostics.NETCore.Client;
+using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace ManagedAttachedNew
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Looking for process...");
+
+            var process = Process.GetProcessesByName("Fibonacci").First();
+
+            Console.WriteLine("Attaching to process...");
+
+            DiagnosticsClient client = new DiagnosticsClient(process.Id);
+            var path = "C:\\Users\\oginiaux\\Projects\\traceman\\bin\\Release\\Profiler.Windows.dll";
+            Guid guid = new Guid("{805A308B-061C-47F3-9B30-F785C3186E82}");
+            client.AttachProfiler(TimeSpan.FromSeconds(10), guid, path, null);
+
+            Console.WriteLine("Attached!");
+
+            Console.ReadLine();
+        }
+    }
+}
