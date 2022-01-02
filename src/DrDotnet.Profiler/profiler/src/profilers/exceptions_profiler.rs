492734e360679d1edd0907f627f6e0e27bf4648d
Olivier Giniaux <oginiaux@smartadserver.com>
2022-01-02 23:39:50 +0100

    There is now a functional proof of concept 🥳

--- src/DrDotnet.Profiler/profiler/src/profilers/exceptions_profiler.rs
+++ src/DrDotnet.Profiler/profiler/src/profilers/exceptions_profiler.rs
@@ -5,9 +5,18 @@ use profiling_api::cil::{nop, Method};
 use std::slice;
 use uuid::Uuid;
 
-#[derive(Clone)]
+use std::fs::File;
+use std::io::Write;
+use crate::report::*;
+
+use std::thread;
+use std::time::Duration;
+
+use std::sync::atomic::{AtomicUsize, Ordering};
+
 pub struct ExceptionsProfiler {
     profiler_info: Option<ProfilerInfo>,
+    exceptions_thrown: AtomicUsize,
 }
 
 impl ExceptionsProfiler {
@@ -16,11 +25,21 @@ impl ExceptionsProfiler {
     }
 }
 
+impl Clone for ExceptionsProfiler {
+    fn clone(&self) -> Self { 
+        ExceptionsProfiler {
+            profiler_info: self.profiler_info.clone(),
+            exceptions_thrown: AtomicUsize::new(0),
+        }
+     }
+}
+
 impl ClrProfiler for ExceptionsProfiler {
 
     fn new() -> ExceptionsProfiler {
         ExceptionsProfiler {
             profiler_info: None,
+            exceptions_thrown: AtomicUsize::new(0),
         }
     }
 
@@ -43,7 +62,7 @@ impl CorProfilerCallback for ExceptionsProfiler {
     }
 
     fn exception_thrown(&mut self, thrown_object_id: ObjectID) -> Result<(), HRESULT> {
-        println!("exception_thrown");
+        self.exceptions_thrown.fetch_add(1, Ordering::Relaxed);
         Ok(())
     }
 }
@@ -64,7 +83,58 @@ impl CorProfilerCallback3 for ExceptionsProfiler {
         println!("initialize with attach");
 
         // Set the event mask
-        self.profiler_info().set_event_mask(COR_PRF_MONITOR::COR_PRF_ALLOWABLE_AFTER_ATTACH)?;
+        //self.profiler_info().set_event_mask(COR_PRF_MONITOR::COR_PRF_ALLOWABLE_AFTER_ATTACH)?;
+        self.profiler_info().set_event_mask(COR_PRF_MONITOR::COR_PRF_MONITOR_EXCEPTIONS)?;
+
+        Ok(())
+    }
+
+    fn profiler_attach_complete(&mut self) -> Result<(), HRESULT> {
+
+        println!("profiler successfully attached!");
+
+        let result = self.profiler_info().request_profiler_detach(10000);
+        println!("detach request result: {:?}", result);
+
+        //thread::spawn(|| {
+        //  thread::sleep(Duration::from_millis(5000));
+        //  self.profiler_info().request_profiler_detach(10);
+        //});
+
+        Ok(())
+    }
+
+    fn profiler_detach_succeeded(&mut self) -> Result<(), HRESULT> {
+
+        println!("profiler successfully detached!");
+
+        let mut entry = ReportEntry {
+            name: "Exceptions".to_owned(),
+            content: format!("exceptions:{}", self.exceptions_thrown.get_mut())
+        };
+
+        let mut section = ReportSection {
+            name: "Exceptions".to_owned(),
+            entries: vec![entry]
+        };
+
+        let mut report = Report{
+            guid: uuid::Uuid::default(),
+            name: String::default(),
+            timestamp: chrono::offset::Local::now(),
+            profiler: ProfilerData::from_profiler::<ExceptionsProfiler>(),
+            sections: vec![section]
+        };
+    
+        report.name = "My Exceptions Report".to_owned();
+    
+        let json = serde_json::to_string_pretty(&report).unwrap();
+        std::fs::create_dir_all("./tmp");
+    
+        let mut f = File::create("./tmp/report.json").expect("Unable to create file");
+        f.write_all(json.as_bytes()).expect("Unable to write data");    
+
+        println!("report written to {}/tmp/report.json", std::env::current_dir().unwrap().display());
 
         Ok(())
     }
