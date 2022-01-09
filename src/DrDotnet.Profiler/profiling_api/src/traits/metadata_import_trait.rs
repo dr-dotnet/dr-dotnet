43c34000a6622b4a60c8a61eb23dc14f97b36431
Olivier Giniaux <oginiaux@gmail.com>
2022-01-09 23:58:41 +0100

    Add CLR API type interfaces

--- src/DrDotnet.Profiler/profiling_api/src/traits/metadata_import_trait.rs
+++ src/DrDotnet.Profiler/profiling_api/src/traits/metadata_import_trait.rs
@@ -1,8 +1,9 @@
 use crate::{
-    ffi::{mdMethodDef, HRESULT},
-    MethodProps,
+    ffi::{mdMethodDef, mdTypeDef, HRESULT},
+    MethodProps, TypeProps
 };
 
 pub trait MetadataImportTrait {
     fn get_method_props(&self, mb: mdMethodDef) -> Result<MethodProps, HRESULT>;
+    fn get_type_def_props(&self, td: mdTypeDef) -> Result<TypeProps, HRESULT>;
 }
