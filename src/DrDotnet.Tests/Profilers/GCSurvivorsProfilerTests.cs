cd33a88d76d8f38c196a0c7f62a543029321ca95
Olivier Giniaux <oginiaux@smartadserver.com>
2022-11-11 23:45:35 +0100

    Fix tests

--- src/DrDotnet.Tests/Profilers/GCSurvivorsProfilerTests.cs
+++ src/DrDotnet.Tests/Profilers/GCSurvivorsProfilerTests.cs
@@ -22,7 +22,7 @@ public class GCSurvivorsProfilerTests : ProfilerTests
         Assert.NotNull(GetProfiler());
     }
 
-    [Test]
+    [Test, Explicit]
     [Order(1)]
     [Timeout(160_000)]
     [NonParallelizable]
