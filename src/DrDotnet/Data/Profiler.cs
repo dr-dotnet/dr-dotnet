2fe49b90e452069c7d3b5d57a4b3af89da572ba2
Olivier Giniaux <oginiaux@gmail.com>
2022-11-17 23:18:39 +0100

    Trying out the diagnotics client differently

--- src/DrDotnet/Data/Profiler.cs
+++ src/DrDotnet/Data/Profiler.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.Diagnostics.NETCore.Client;
+using Microsoft.Diagnostics.Tracing;
+using Microsoft.Diagnostics.Tracing.Parsers;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -33,6 +37,43 @@ public class Profiler
         logger.Log($"Profiler DLL path is '{profilerDll}'. Exists: {File.Exists(profilerDll)}");
 
         DiagnosticsClient client = new DiagnosticsClient(processId);
+
+        try {
+            var env = client.GetProcessEnvironment();
+            foreach (var pair in env) {
+                logger.Log($"- Env name: {pair.Key}, value: {pair.Value}");
+            }
+        } catch(Exception e) {
+            logger.Log($"[ERROR] Error while retreiving env variables: {e}");
+        }
+
+        try {
+            var providers = new List<EventPipeProvider>()
+            {
+                new EventPipeProvider("Microsoft-Windows-DotNETRuntime",
+                    EventLevel.Informational, (long)ClrTraceEventParser.Keywords.GC)
+            };
+            using (var session = client.StartEventPipeSession(providers, false)) {
+                var source = new EventPipeEventSource(session.EventStream);
+
+                source.Clr.All += (TraceEvent obj) => {
+                    logger.Log("ETW EVENT: " + obj.EventName);
+                };
+
+                try {
+                    source.Process();
+                }
+                // NOTE: This exception does not currently exist. It is something that needs to be added to TraceEvent.
+                catch (Exception e) {
+                    logger.Log("Error encountered while processing events");
+                    logger.Log(e.ToString());
+                }
+            }
+        }
+        catch (Exception e) {
+            logger.Log($"[ERROR] Error while retreiving etw runtime events: {e}");
+        }
+
         client.AttachProfiler(TimeSpan.FromSeconds(10), ProfilerId, profilerDll, Encoding.UTF8.GetBytes(sessionId.ToString() + "\0"));
 
         logger.Log($"Attached profiler {ProfilerId} with session {sessionId} to process {processId}");
