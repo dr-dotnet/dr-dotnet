ed843ab1fabe975849ed64465c7bd1b730fc535c
Olivier Giniaux <oginiaux@gmail.com>
2023-01-21 18:21:53 +0100

    Try without ComImport

--- src/DrDotnet.Tests/Services/ProfilersDiscoveryTests.cs
+++ src/DrDotnet.Tests/Services/ProfilersDiscoveryTests.cs
@@ -91,7 +91,6 @@ public class ProfilersDiscoveryTests
 
     [Guid("00000001-0000-0000-c000-000000000046")]
     [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-    [ComImport]
     internal interface IClassFactory
     {
         void CreateInstance([MarshalAs(UnmanagedType.IUnknown)] object pUnkOuter, ref Guid riid, [MarshalAs(UnmanagedType.IUnknown)] out object ppvObject);
