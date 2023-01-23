355d092bab2ca20fbf0477bdf1fbb1b460f78cc5
Olivier Giniaux <oginiaux@gmail.com>
2023-01-23 11:53:28 +0100

    Parametrize test

--- src/DrDotnet.Tests/SegfaultReproTests.cs
+++ src/DrDotnet.Tests/SegfaultReproTests.cs
@@ -5,6 +5,7 @@ using System.Threading;
 using Microsoft.Diagnostics.NETCore.Client;
 using System.Text;
 using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace DrDotnet.Tests;
 
@@ -13,20 +14,31 @@ public class SegfaultReproTests
     private delegate int DllGetClassObject(ref Guid clsid, ref Guid iid, out nint classFactoryPtr);
     private delegate void CreateInstance(nint self, nint pUnkOuter, ref Guid riid, out nint ppvObjectPtr);
 
-    [Test]
+    private static IEnumerable<TestCaseData> GetProfilerPath() {
+        yield return new TestCaseData(Profiler.GetLocalProfilerLibrary());
+        yield return new TestCaseData(Profiler.GetTmpProfilerLibrary());
+    }
+
+    [Test, TestCaseSource(nameof(GetProfilerPath))]
     [Order(1)]
     [NonParallelizable]
-    public unsafe void Load_And_Create_Profiler_Instance_Manually()
+    public unsafe void Load_And_Create_Profiler_Instance_Manually(string profilerPath)
     {
         Guid exceptionProfilerGuid = new Guid("805A308B-061C-47F3-9B30-F785C3186E82");
         Guid iclassFactoryGuid = new Guid("00000001-0000-0000-c000-000000000046");
         Guid iCorProfilerCallback8Guid = new Guid("5BED9B15-C079-4D47-BFE2-215A140C07E0");
 
+        Console.WriteLine($"[{nameof(Load_And_Create_Profiler_Instance_Manually)}] Profiler path: {profilerPath}");
+        Debug.WriteLine($"[{nameof(Load_And_Create_Profiler_Instance_Manually)}] Profiler path: {profilerPath}");
+        Trace.WriteLine($"[{nameof(Load_And_Create_Profiler_Instance_Manually)}] Profiler path: {profilerPath}");
+
         for (int i = 0; i < 3; i++) {
             Console.WriteLine($"[{nameof(Load_And_Create_Profiler_Instance_Manually)}] Iteration {i}");
+            Debug.WriteLine($"[{nameof(Load_And_Create_Profiler_Instance_Manually)}] Iteration {i}");
+            Trace.WriteLine($"[{nameof(Load_And_Create_Profiler_Instance_Manually)}] Iteration {i}");
 
             // Load profilers library (dlopen on linux)
-            Assert.True(NativeLibrary.TryLoad(Profiler.GetTmpProfilerLibrary(), typeof(SegfaultReproTests).Assembly, DllImportSearchPath.AssemblyDirectory, out nint handle));
+            Assert.True(NativeLibrary.TryLoad(profilerPath, typeof(SegfaultReproTests).Assembly, DllImportSearchPath.AssemblyDirectory, out nint handle));
             Assert.AreNotEqual(nint.Zero, handle);
 
             // Get pointer to method DllGetClassObject (dlsym on linux)
@@ -55,57 +67,29 @@ public class SegfaultReproTests
         }
     }
 
-    [Test]
+    [Test, TestCaseSource(nameof(GetProfilerPath))]
     [Order(2)]
     [NonParallelizable]
-    public void Attach_Using_DiagnosticsClient()
-    {
+    public void Attach_Using_DiagnosticsClient(string profilerPath) {
         Guid exceptionProfilerGuid = new Guid("805A308B-061C-47F3-9B30-F785C3186E82");
 
-        for (int i = 0; i < 3; i++)
-        {
-            Console.WriteLine($"[{nameof(Attach_Using_DiagnosticsClient)}] Iteration {i}");
-
-            int processId = Process.GetCurrentProcess().Id;
-            DiagnosticsClient client = new DiagnosticsClient(processId);
-
-            Guid sessionId = Guid.NewGuid();
-
-            string profilerDll = Profiler.GetLocalProfilerLibrary();
-
-            Console.WriteLine($"[{nameof(Attach_Using_DiagnosticsClient)}] Profiler path: '{profilerDll}'");
-
-            Console.Out.Flush();
-
-            client.AttachProfiler(TimeSpan.FromSeconds(10), exceptionProfilerGuid, profilerDll, Encoding.UTF8.GetBytes(sessionId.ToString() + "\0"));
-
-            // This profiler detaches automatically after about 10s
-            Thread.Sleep(12_000);
-        }
-    }
-
-    [Test]
-    [Order(3)]
-    [NonParallelizable]
-    public void Attach_Using_DiagnosticsClient_TempDir()
-    {
-        Guid exceptionProfilerGuid = new Guid("805A308B-061C-47F3-9B30-F785C3186E82");
+        Console.WriteLine($"[{nameof(Attach_Using_DiagnosticsClient)}] Profiler path: {profilerPath}");
+        Debug.WriteLine($"[{nameof(Attach_Using_DiagnosticsClient)}] Profiler path: {profilerPath}");
+        Trace.WriteLine($"[{nameof(Attach_Using_DiagnosticsClient)}] Profiler path: {profilerPath}");
 
         for (int i = 0; i < 3; i++) {
-            Console.WriteLine($"[{nameof(Attach_Using_DiagnosticsClient_TempDir)}] Iteration {i}");
+            Console.WriteLine($"[{nameof(Attach_Using_DiagnosticsClient)}] Iteration {i}");
+            Debug.WriteLine($"[{nameof(Attach_Using_DiagnosticsClient)}] Iteration {i}");
+            Trace.WriteLine($"[{nameof(Attach_Using_DiagnosticsClient)}] Iteration {i}");
 
             int processId = Process.GetCurrentProcess().Id;
             DiagnosticsClient client = new DiagnosticsClient(processId);
 
             Guid sessionId = Guid.NewGuid();
 
-            string profilerDll = Profiler.GetTmpProfilerLibrary();
-
-            Console.WriteLine($"[{nameof(Attach_Using_DiagnosticsClient)}] Profiler path: '{profilerDll}'");
-
             Console.Out.Flush();
 
-            client.AttachProfiler(TimeSpan.FromSeconds(10), exceptionProfilerGuid, profilerDll, Encoding.UTF8.GetBytes(sessionId.ToString() + "\0"));
+            client.AttachProfiler(TimeSpan.FromSeconds(10), exceptionProfilerGuid, profilerPath, Encoding.UTF8.GetBytes(sessionId.ToString() + "\0"));
 
             // This profiler detaches automatically after about 10s
             Thread.Sleep(12_000);
