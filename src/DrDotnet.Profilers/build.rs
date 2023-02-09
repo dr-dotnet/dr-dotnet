02508f0d5b6c7ac3f08ba46670a61dfe173158a6
Olivier Giniaux <oginiaux@smartadserver.com>
2023-02-09 22:52:45 +0100

    Start using protobuf instead of ffidji

--- src/DrDotnet.Profilers/build.rs
+++ src/DrDotnet.Profilers/build.rs
@@ -1,3 +1,5 @@
+
+
 fn main() {
     // For debugging purpose:
     //panic!("[build.rs] VERSION={:?}", std::env::var("VERSION"));
@@ -5,4 +7,11 @@ fn main() {
     if let Ok(val) = std::env::var("VERSION") {
         println!("cargo:rustc-env=CARGO_PKG_VERSION={}", val);
     }
+
+    protobuf_codegen::Codegen::new()
+        .pure()
+        .include("protos")
+        .input("protos/triangle.proto")
+        .cargo_out_dir("rust_protobuf_protos")
+        .run_from_script();
 }
--- src/DrDotnet.Profilers/lib.rs
+++ src/DrDotnet.Profilers/lib.rs
@@ -11,6 +11,10 @@ extern crate bitflags;
 #[macro_use]
 extern crate log;
 
+mod rust_protobuf_protos {
+    include!(concat!(env!("OUT_DIR"), "/rust_protobuf_protos/mod.rs"));
+}
+
 // Create function to list and attach profilers
 register!(
     GCSurvivorsProfiler,
@@ -25,6 +29,10 @@ register!(
 #[no_mangle]
 unsafe extern "system" fn DllGetClassObject(rclsid: ffi::REFCLSID, riid: ffi::REFIID, ppv: *mut ffi::LPVOID) -> ffi::HRESULT
 {
+    let mut rp = rust_protobuf_protos::triangle::Triangle::new();
+    rp.description = "The triangle".to_owned();
+    rp.color = rust_protobuf_protos::triangle::Color::RED.into();
+
     profilers::init_logging();
 
     debug!("DllGetClassObject(rclsid: {:?}, riid: {:?})", rclsid, riid);
